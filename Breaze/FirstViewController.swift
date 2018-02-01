1826abda57830050e6ea4ccd9cd95253aa82c509
joehardy3030 <jlhardyphd@gmail.com>
2018-01-31 20:36:09 -0800

    Initial Commit

---
+++ Breaze/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Breaze
+//
+//  Created by Joseph Hardy on 1/31/18.
+//  Copyright © 2018 Carquinez. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

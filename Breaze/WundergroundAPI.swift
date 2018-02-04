d5d0442bfcee4d271f5586ced4163ff5f36edf25
joehardy3030 <jlhardyphd@gmail.com>
2018-02-04 14:26:39 -0800

    Added the S to the HTTPS and now the WundergroundAPI clears app layer transport security. LOL

--- Breaze/WundergroundAPI.swift
+++ Breaze/WundergroundAPI.swift
@@ -10,16 +10,18 @@ import Foundation
 
 enum Method: String {
     // dataUrl = @"http://api.wunderground.com/api/ffd1b93b6a497308/conditions/forecast/q/CA/El_Cerrito.json";
-    case simpleForecast = "conditions/forecast/q/"
+    //case simpleForecast = "conditions/forecast/q/"
+    case simpleForecast = "https://api.wunderground.com/api/ffd1b93b6a497308/conditions/forecast/q/CA/El_Cerrito.json"
 }
 
 struct WundergroundAPI {
     // This is the base URL for the API
-    private static let baseURLString = "http://api.wunderground.com/api/"
-    private static let apiKey = "ffd1b93b6a497308"
+  //  private static let baseURLString = "http://api.wunderground.com/api/"
+   // private static let apiKey = "ffd1b93b6a497308"
 
     static var simpleForecastURL: URL {
-        return wundergroundURL(method: .simpleForecast, parameters: ["extras": "url_h,date_taken"])
+        // return wundergroundURL(method: .simpleForecast, parameters: ["extras": "url_h,date_taken"])
+        return wundergroundURL(method: .simpleForecast, parameters: nil)
     }
 
     // This private function returns the URL
@@ -27,28 +29,30 @@ struct WundergroundAPI {
     // as well as a set of optional dictionory of query parameters
     private static func wundergroundURL(method: Method, parameters: [String:String]?) -> URL
     {
-        var components = URLComponents(string: baseURLString)!
+        let components = URLComponents(string: method.rawValue)!
         
-        var queryItems = [URLQueryItem]()
+    //    var components = URLComponents(string: baseURLString)!
         
-        let baseParams = ["method": method.rawValue,
-                          "format": "json",
-                          "nojsoncallback": "1",
-                          "api_key": apiKey]
+     //   var queryItems = [URLQueryItem]()
         
-        for (key, value) in baseParams {
-            let item = URLQueryItem(name: key, value: value)
-            queryItems.append(item)
-        }
+      //  let baseParams = ["method": method.rawValue,
+        //                  "format": "json",
+         //                 "nojsoncallback": "1",
+          //                "api_key": apiKey]
         
-        if let additionalParams = parameters {
-            for (key, value) in additionalParams {
-                let item = URLQueryItem(name: key, value: value)
-                queryItems.append(item)
-            }
-        }
-        components.queryItems = queryItems
+      //  for (key, value) in baseParams {
+       //     let item = URLQueryItem(name: key, value: value)
+        //    queryItems.append(item)
+       // }
         
+       // if let additionalParams = parameters {
+        //    for (key, value) in additionalParams {
+        //        let item = URLQueryItem(name: key, value: value)
+         //       queryItems.append(item)
+          //  }
+      //  }
+        //components.queryItems = queryItems
+        print(components.url!)
         return components.url!
         
     }

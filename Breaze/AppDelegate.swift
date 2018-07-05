47d55e6498aab510cd885dc875ea99b05f365562
Joe Hardy <joehardy_1974@yahoo.com>
2018-07-04 19:05:16 -0700

    Save lastLocation in database using Core Data, when you update the location manager.

--- Breaze/AppDelegate.swift
+++ Breaze/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import CoreLocation
+import CoreData
 
 //let carquinezNotificationKey = "com.carquinez.notificationKey"
 extension Notification.Name {
@@ -32,6 +33,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate, CLLocationManagerDelegate
         return true
     }
 
+   /* private static func saveCurrentLocation(context: NSManagedObjectContext) {
+        var lastLocation = LastLocation!
+        context.performAndWait {
+            lastLocation = LastLocation()
+            
+        }
+    }
+*/
     func locationManager(_ manager: CLLocationManager,
                          didFailWithError error: Error) {
         print("error")
@@ -39,11 +48,20 @@ class AppDelegate: UIResponder, UIApplicationDelegate, CLLocationManagerDelegate
     
     func locationManager(_ manager: CLLocationManager,
                          didUpdateLocations locations: [CLLocation]){
+        //let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = self.persistentContainer.viewContext
+        let entity = NSEntityDescription.entity(forEntityName: "LastLocation", in: context)
+        let lastLocation = NSManagedObject(entity: entity!, insertInto: context)
         
         self.currentLocation = locations.last
+        lastLocation.setValue(String(self.currentLocation.coordinate.latitude), forKey: "latitude")
+        lastLocation.setValue(String(self.currentLocation.coordinate.latitude), forKey: "longitude")
+        lastLocation.setValue(Date(), forKey: "dateSaved")
+        
         DispatchQueue.main.async{
             NotificationCenter.default.post(name: .alocation, object: nil)
             print("location Updated")
+            self.saveContext()
         }
     }
     
@@ -67,7 +85,54 @@ class AppDelegate: UIResponder, UIApplicationDelegate, CLLocationManagerDelegate
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        self.saveContext()
     }
+    
+    // MARK: - Core Data stack
+    
+    lazy var persistentContainer: NSPersistentContainer = {
+        /*
+         The persistent container for the application. This implementation
+         creates and returns a container, having loaded the store for the
+         application to it. This property is optional since there are legitimate
+         error conditions that could cause the creation of the store to fail.
+         */
+        let container = NSPersistentContainer(name: "Weather")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                
+                /*
+                 Typical reasons for an error here include:
+                 * The parent directory does not exist, cannot be created, or disallows writing.
+                 * The persistent store is not accessible, due to permissions or data protection when the device is locked.
+                 * The device is out of space.
+                 * The store could not be migrated to the current model version.
+                 Check the error message to determine what the actual problem was.
+                 */
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+    
+    // MARK: - Core Data Saving support
+    
+    func saveContext () {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+
 
 
 }
---
+++ Breaze/LastLocation+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  LastLocation+CoreDataClass.swift
+//  Breaze
+//
+//  Created by Joe Hardy on 6/28/18.
+//  Copyright © 2018 Carquinez. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(LastLocation)
+public class LastLocation: NSManagedObject {
+
+}

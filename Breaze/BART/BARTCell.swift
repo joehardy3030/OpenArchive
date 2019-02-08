78315438d61eda8d97c3b4b14cb0d283b0f24430
Joe Hardy <joehardy@joes-macbook-pro-3.local>
2019-02-07 20:44:05 -0800

    Added BART tab

---
+++ Breaze/BART/BARTCell.swift
@@ -0,0 +1,23 @@
+//
+//  BARTCell.swift
+//  Breaze
+//
+//  Created by Joe Hardy on 2/7/19.
+//  Copyright © 2019 Carquinez. All rights reserved.
+//
+
+import UIKit
+
+class BARTCell: UITableViewCell {
+    
+    @IBOutlet var numCarsLabel: UILabel!
+    @IBOutlet var minToArrivalLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        //highTempLabel.adjustsFontForContentSizeCategory = true
+       // lowTempLabel.adjustsFontForContentSizeCategory = true
+        //iconLabel.adjustsFontForContentSizeCategory = true
+    }
+}

f52daa901ba1c29dfa3791bbd3b83a6ebb49899b
Joe Hardy <jlhardyphd@gmail.com>
2020-06-24 21:01:56 -0700

    Adding navigation endpoint for archive view controller

---
+++ Breaze/Archive/ArchiveViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ArchiveViewController.swift
+//  Breaze
+//
+//  Created by Joe Hardy on 6/24/20.
+//  Copyright © 2020 Carquinez. All rights reserved.
+//
+
+import UIKit
+
+class ArchiveViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

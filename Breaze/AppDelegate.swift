0c4b54f4177b45df81a135351cd4d355c6d5032a
Joe Hardy <jlharyphd@gmail.com>
2020-07-17 12:12:43 -0700

    remove all the files

--- Breaze/AppDelegate.swift
+++ Breaze/AppDelegate.swift
@@ -17,19 +17,10 @@ import Firebase
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-    var currentLocation: CLLocation!
-    let locationManager = CLLocationManager()
     let center = UNUserNotificationCenter.current()
-    static let geoCoder = CLGeocoder()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        
-        self.locationManager.delegate = self
-        self.locationManager.requestAlwaysAuthorization()
-        if CLLocationManager.locationServicesEnabled() {
-            self.locationManager.startMonitoringVisits()
-        }
         center.requestAuthorization(options: [.alert, .sound]) { granted, error in }
         FirebaseApp.configure()
        // UIApplication.shared.beginReceivingRemoteControlEvents()
@@ -41,105 +32,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
       //  self.saveContext()
     }
     
-    // MARK: - Core Data stack
-    /*
-    lazy var persistentContainer: NSPersistentContainer = {
-        /*
-         The persistent container for the application. This implementation
-         creates and returns a container, having loaded the store for the
-         application to it. This property is optional since there are legitimate
-         error conditions that could cause the creation of the store to fail.
-         */
-        let container = NSPersistentContainer(name: "Weather")
-        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
-            if let error = error as NSError? {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                
-                /*
-                 Typical reasons for an error here include:
-                 * The parent directory does not exist, cannot be created, or disallows writing.
-                 * The persistent store is not accessible, due to permissions or data protection when the device is locked.
-                 * The device is out of space.
-                 * The store could not be migrated to the current model version.
-                 Check the error message to determine what the actual problem was.
-                 */
-                fatalError("Unresolved error \(error), \(error.userInfo)")
-            }
-        })
-        return container
-    }()
-    
-    // MARK: - Core Data Saving support
-    
-    func saveContext () {
-        let context = persistentContainer.viewContext
-        if context.hasChanges {
-            do {
-                try context.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nserror = error as NSError
-                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
-            }
-        }
-    }
-    */
 }
 
-
-extension AppDelegate: CLLocationManagerDelegate {
-    
-    func locationManager(_ manager: CLLocationManager,
-                         didFailWithError error: Error) {
-        print("App delegate location error")
-        
-    }
-    
-    func locationManager(_ manager: CLLocationManager,
-                         didUpdateLocations locations: [CLLocation]){
-        let cl = CurrentLocation()
-        self.locationManager.stopUpdatingLocation()
-        guard let location = manager.location else { return }
-        self.currentLocation = location
-        cl.locationNotification(location: location)
-        print("App delegate updated location")
-    }
-    
-    func locationManager(_ manager: CLLocationManager, didVisit visit: CLVisit) {
-        // create CLLocation from the coordinates of CLVisit
-        let clLocation = CLLocation(latitude: visit.coordinate.latitude, longitude: visit.coordinate.longitude)
-        self.currentLocation = clLocation
-        
-        // Get location description
-        AppDelegate.geoCoder.reverseGeocodeLocation(clLocation) { placemarks, _ in
-          if let place = placemarks?.first {
-            let description = "\(place)"
-            self.newVisitReceived(visit, description: description)
-          }
-        }
-
-    }
-    
-    func newVisitReceived(_ visit: CLVisit, description: String) {
-        let location = Location(visit: visit, descriptionString: description)
-        LocationsStorage.shared.saveLocationOnDisk(location)
-
-        // 1
-        let content = UNMutableNotificationContent()
-        content.title = "New Journal entry 📌"
-        content.body = location.description
-        content.sound = .default
-
-        // 2
-        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 1, repeats: false)
-        let request = UNNotificationRequest(identifier: location.dateString, content: content, trigger: trigger)
-
-        // 3
-        center.add(request, withCompletionHandler: nil)
-
-        // Save location to disk        
-
-    }
-}

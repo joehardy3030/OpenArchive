8ffe29948331c6ec54c9b065e63d5a26b9b16975
Joe Hardy <jlharyphd@gmail.com>
2020-08-02 21:09:14 -0700

    Play/pause works on modal view controller

--- ViewControllers/ModalPlayerViewController.swift
+++ ViewControllers/ModalPlayerViewController.swift
@@ -10,13 +10,73 @@ import UIKit
 
 class ModalPlayerViewController: ArchiveSuperViewController {
 
+    @IBOutlet weak var playButton: UIButton!
     @IBOutlet weak var modalPlayerTableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        setupPlayer()
+    }
+
+    @IBAction func forwardButton(_ sender: Any) {
+        if let q = player?.playerQueue {
+            q.advanceToNextItem()
+        }
+    }
+    
+    @IBAction func playButton(_ sender: Any) {
+        playPause()
+    }
+        
+    func setupPlayer() {
+        playPauseButtonImageSetup()
+    }
+    
+    func playPauseButtonImageSetup() {
+        guard let q = player?.playerQueue else { return }
+        if q.rate > 0.0 {
+            if let _ = playButton {
+                if #available(iOS 13.0, *) {
+                    playButton.setBackgroundImage(UIImage(systemName: "pause.circle.fill"), for: .normal)
+                }
+            }
+        //    isPlaying = false
+        }
+        else {
+            if let _ = playButton {
+                if #available(iOS 13.0, *) {
+                    playButton.setBackgroundImage(UIImage(systemName: "play.circle.fill"), for: .normal)
+                }
+            }
+         //   isPlaying = true
+        }
+    }
+    
+    func playPause() {
+        guard let q = player?.playerQueue else { return }
+        if q.rate > 0.0 {
+            q.pause()
+            if let _ = playButton {
+                if #available(iOS 13.0, *) {
+                    playButton.setBackgroundImage(UIImage(systemName: "play.circle.fill"), for: .normal)
+                }
+            }
+        //    isPlaying = false
+        }
+        else {
+            q.play()
+            if let _ = playButton {
+                if #available(iOS 13.0, *) {
+                    playButton.setBackgroundImage(UIImage(systemName: "pause.circle.fill"), for: .normal)
+                }
+            }
+         //   isPlaying = true
+        }
+    }
 
     /*
     // MARK: - Navigation

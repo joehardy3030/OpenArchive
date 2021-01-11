173cad7027cae0320ec961b127032b9302f51731
Joe Hardy <jlharyphd@gmail.com>
2021-01-10 19:39:39 -0800

    Lots of broken stuff in added car play

---
+++ Breaze/CarPlaySceneDelegate.swift
@@ -0,0 +1,41 @@
+//
+//  CarPlaySceneDelegate.swift
+//  Breaze
+//
+//  Created by Joseph Hardy on 1/10/21.
+//  Copyright © 2021 Carquinez. All rights reserved.
+//
+
+import Foundation
+import CarPlay
+
+class CarPlaySceneDelegate: UIResponder, CPTemplateApplicationSceneDelegate {
+    var interfaceController: CPInterfaceController?
+    
+    // CarPlay connected
+    @available(iOS 13.0, *)
+    func templateApplicationScene(_ templateApplicationScene: CPTemplateApplicationScene,
+                                  didConnect interfaceController: CPInterfaceController) {
+        self.interfaceController = interfaceController
+        
+        let item = CPListItem(text: "My title", detailText: "My subtitle")
+        //item.listItemHandler = { item, completion, [weak self] in
+         // Start playback asynchronously…
+        // self.interfaceController.pushTemplate(CPNowPlayingTemplate.shared(), animated: true)
+        // completion()
+        //}
+        let section = CPListSection(items: [item])
+        let listTemplate = CPListTemplate(title: "Albums", sections: [section])
+        self.interfaceController?.pushTemplate(listTemplate, animated: true)
+
+        
+        //let listTemplate: CPListTemplate = CPListTemplate()
+        interfaceController.setRootTemplate(listTemplate, animated: true)
+    }
+    // CarPlay disconnected
+    @available(iOS 13.0, *)
+    private func templateApplicationScene(_ templateApplicationScene: CPTemplateApplicationScene,
+                                  didDisconnect interfaceController: CPInterfaceController) {
+        self.interfaceController = nil
+    }
+}

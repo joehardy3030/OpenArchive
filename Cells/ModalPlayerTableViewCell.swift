cb15d09e9bf01cb9ba2926541e71e013aee98f69
Joe Hardy <jlharyphd@gmail.com>
2025-06-04 20:39:20 -0700

    Made all the fonts a bit bigger and used the AppFonts for the ModalPlayerTableViewCell

--- Cells/ModalPlayerTableViewCell.swift
+++ Cells/ModalPlayerTableViewCell.swift
@@ -3,16 +3,34 @@
 //  Breaze
 //
 //  Created by Joseph Hardy on 8/2/20.
-//  Copyright © 2020 Carquinez. All rights reserved.
+//  Copyright 2020 Carquinez. All rights reserved.
 //
 
 import UIKit
 
 class ModalPlayerTableViewCell: UITableViewCell {
 
+    // Called when the cell is created programmatically
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        commonInit()
+    }
+
+    // Called when the cell is loaded from a storyboard or XIB
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        // awakeFromNib will be called after this, so commonInit() is called there
+    }
+
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        commonInit()
+    }
+
+    private func commonInit() {
+        // Ensure the style allows for a textLabel if you're relying on the default one.
+        // If you're using a custom style from a XIB without a textLabel outlet, this might be nil.
+        textLabel?.font = AppFonts.bodyFont // Using AppFonts.bodyFont as requested
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
--- Utilities/AppFonts.swift
+++ Utilities/AppFonts.swift
@@ -2,9 +2,9 @@ import UIKit
 
 class AppFonts {
     static let titleFont: UIFont = .systemFont(ofSize: 18, weight: .bold)
-    static let subtitleBoldFont: UIFont = .systemFont(ofSize: 16, weight: .bold)
-    static let bodyFont: UIFont = .systemFont(ofSize: 15)
-    static let captionFont: UIFont = .systemFont(ofSize: 14)
+    static let subtitleBoldFont: UIFont = .systemFont(ofSize: 17, weight: .bold)
+    static let bodyFont: UIFont = .systemFont(ofSize: 16)
+    static let captionFont: UIFont = .systemFont(ofSize: 15)
 
     // Private initializer to prevent instantiation
     private init() {}
